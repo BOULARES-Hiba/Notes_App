108fe0c012a95e6155898cbc06c601f39f10f8ce
BOULARES-Hibs <hibablr.info@gmail.com>
2025-08-22 12:04:34 +0100

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-600 p-4'>hii</div>
+       
+    </>
+  )
+}
+
+export default App
