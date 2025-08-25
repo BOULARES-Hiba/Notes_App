c1d546f0fde9a04f52a7048294de8ec05c924ba8
BOULARES-Hibs <hibablr.info@gmail.com>
2025-08-25 15:38:14 +0100

    correct cookie

--- server/controllers/auth/authController.js
+++ server/controllers/auth/authController.js
@@ -94,7 +94,6 @@ export const login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", 
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      sameSite: "strict",
       maxAge: COOKIE_MAX_AGE
     }).json({
       success: true,
